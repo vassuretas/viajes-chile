13a6d21e2bb116ba516799a38722725d6464e581
Bastian <vass.uretas@gmail.com>
2021-04-14 03:23:30 -0400

    Se remplazaron las imagenes, optimizadas en tamaño , agregue smoth scrolling pero solo con css, en js- se agregaron eventos de animacion a iconos de seccion quienes somos.

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,62 +11,50 @@ $(function () {
 
 // scrool smoth
 
-		 $("a").click(function(event){
-		 		if (this.hash !== ""){
-		 		 event.preventDefault();
+		//  $("a").click(function(event){
+		//  		if (this.hash !== ""){
+		//  		 event.preventDefault();
 
-			var gato = this.hash;
+		// 	var gato = this.hash;
 
-			$("html, body").animate({
-				scrollTop: $(gato).offset().top
-			}, 800, function(){
-				window.location.hash = gato;
-			});
-		}
+		// 	$("html, body").animate({
+		// 		scrollTop: $(gato).offset().top
+		// 	}, 800, function(){
+		// 		window.location.hash = gato;
+		// 	});
+		// }
 
-		 });
+		//  });
 
-
-  // $('[data-toggle="tooltip"]').tooltip()
-
-
-//   $("#enviarCorreo").click(function (){
-//   alert("El correo fue enviado correctamente...");
-//   });
-
-//   $("#agregarFavoritos").click(function (){
-//   alert("¡Felicidades! La receta se ha agregado a tus favoritos.");
-//   });
+		$('[data-toggle="popover"]').popover()
+  		$('[data-toggle="tooltip"]').tooltip()
 
 
-//   $("h5").dblclick(function(){
-//   	$(this).css("color", "red");
-//   });
+// Animacion Botones quienes somos
 
 
-// var ocultar = $(".card").children().find("p");
-// console.log( "llama a ocultar--->", ocultar);
-// var textcard1 = ocultar.eq(0).hide();
-// var textcard2 = ocultar.eq(1).hide();
-// var textcard3 = ocultar.eq(2).hide();
+var ocultar = $(".card-qs").children().find("p");
+console.log( "llama a ocultar--->", ocultar);
+var textcard1 = ocultar.eq(0).hide();
+var textcard2 = ocultar.eq(1).hide();
+var textcard3 = ocultar.eq(2).hide();
 
-// var cardtitle = $(".card").children().find("h4");
-// console.log( "llama a cardtitle--->", cardtitle);
-// var titlecard1 = cardtitle.eq(0);
-// var titlecard2 = cardtitle.eq(1);
-// var titlecard3 = cardtitle.eq(2);
+var cardtitle = $(".card-qs").children().find("i");
+console.log( "llama a cardtitle--->", cardtitle);
+var titlecard1 = cardtitle.eq(0);
+var titlecard2 = cardtitle.eq(1);
+var titlecard3 = cardtitle.eq(2);
 
-// titlecard1.click(function(event){
-// 	textcard1.toggle("fast");
-// });
+titlecard1.click(function(event){
+	textcard1.toggle("fast");
+});
 
-// titlecard2.click(function(event){
-// 	textcard2.toggle("fast");
-// });
-// titlecard3.click(function(event){
-// 	textcard3.toggle("fast");
-// });
-
+titlecard2.click(function(event){
+	textcard2.toggle("fast");
+});
+titlecard3.click(function(event){
+	textcard3.toggle("fast");
+});
 
 
 });
