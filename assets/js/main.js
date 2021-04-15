d9e1809f99b44dd50fdf8bfc92b5ea0c78027164
Bastian <vass.uretas@gmail.com>
2021-04-15 19:08:23 -0400

    se agrego a git los archivos modificados

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,28 +33,28 @@ $(function () {
 // Animacion Botones quienes somos
 
 
-var ocultar = $(".card-qs").children().find("p");
-console.log( "llama a ocultar--->", ocultar);
-var textcard1 = ocultar.eq(0).hide();
-var textcard2 = ocultar.eq(1).hide();
-var textcard3 = ocultar.eq(2).hide();
+		var ocultar = $(".card-qs").children().find("p");
+		console.log( "llama a ocultar--->", ocultar);
+		var textcard1 = ocultar.eq(0).hide();
+		var textcard2 = ocultar.eq(1).hide();
+		var textcard3 = ocultar.eq(2).hide();
 
-var cardtitle = $(".card-qs").children().find("i");
-console.log( "llama a cardtitle--->", cardtitle);
-var titlecard1 = cardtitle.eq(0);
-var titlecard2 = cardtitle.eq(1);
-var titlecard3 = cardtitle.eq(2);
+		var cardtitle = $(".card-qs").children().find("i");
+		console.log( "llama a cardtitle--->", cardtitle);
+		var titlecard1 = cardtitle.eq(0);
+		var titlecard2 = cardtitle.eq(1);
+		var titlecard3 = cardtitle.eq(2);
 
-titlecard1.click(function(event){
-	textcard1.toggle("fast");
-});
+		titlecard1.click(function(event){
+			textcard1.toggle("fast");
+		});
 
-titlecard2.click(function(event){
-	textcard2.toggle("fast");
-});
-titlecard3.click(function(event){
-	textcard3.toggle("fast");
-});
+		titlecard2.click(function(event){
+			textcard2.toggle("fast");
+		});
+		titlecard3.click(function(event){
+			textcard3.toggle("fast");
+		});
 
 
 });
